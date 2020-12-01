58ccb1020daa21014a79ad2dd593fdba1c4b708a
kazscape <kazscape@gmail.com>
2020-12-01 18:59:39 +0900

    [modified] MesloLGS NFフォントのインストールを追加
    MesloLGS NFフォントを所定のURIからダウンロードして、システムにインストールするように変更
    インストール後はダウンロードしたファイルを削除するよう変更

--- windows_setup_pwsh.ps1
+++ windows_setup_pwsh.ps1
@@ -34,8 +34,25 @@ scoop install pwsh
 # Windows Terminalのインストール
 scoop install windows-terminal
 
+# Winmergeのインストール
+scoop install winmerge
+
 # キャッシュのクリア
 scoop cache rm *
+
+# MesloLGS NFフォントのダウンロード
+Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"  -Outfile ".\MesloLGS NF Regular.ttf"
+Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"  -Outfile ".\MesloLGS NF Bold.ttf"
+Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"  -Outfile ".\MesloLGS NF Italic.ttf"
+Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"  -Outfile ".\MesloLGS NF Bold Italic.ttf"
+
+# フォントのインストール
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+dir .\*.ttf | %{ $fonts.CopyHere($_.fullname) }
+
+# ファイルの削除
+Remove-Item .\*.ttf
+
 
 # ログファイルの出力停止
 Stop-Transcript
