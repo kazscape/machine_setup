40c82b55a48316330016c21d9a25ad70ca67e9c6
kazscape <kazscape@gmail.com>
2020-12-01 12:38:51 +0900

    [modified] scoopのインストール
    パッケージ管理ツール「scoop」のインストールを追加

--- windows_setup_pwsh.ps1
+++ windows_setup_pwsh.ps1
@@ -2,12 +2,16 @@
 Start-Transcript "./windows_setup_pwsh_log.txt"
 
 # 実行前のセキュリティポリシーの取得
-echo "Before Set-ExecutionPolily"
 Get-ExecutionPolicy -List
 
 # 実行ポリシーを変更する
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser -Force
 
 # 実行前のセキュリティポリシーの取得
-echo "`r`nAfter Set-ExecutionPolily"
 Get-ExecutionPolicy -List
+
+# scoopのインストール
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+# ログファイルの出力停止
+Stop-Transcript
