26561e184fb137896dafd8fe72c65cf1dca25441
kazscape <kazscape@gmail.com>
2020-12-03 12:30:40 +0900

    [new] oh-my-poshをpowershell coreで実行するためのスクリプトを追加
    windows_setup_pwsh.ps1は、Windows powershellで実行されるため、同スクリプト中にoh-my-poshの設定を組み込むと、Windows powershell側に設定されてしまう。そのため、oh-my-poshの設定用スクリプトは別ファイルとして切り出し、pwshコマンドで実行できるようにする。

---
+++ setup_oh-my-posh.ps1
@@ -0,0 +1,15 @@
+﻿# ユーザフォルダパスの取得
+$userprofile = $env:USERPROFILE
+
+# PSGalleryの信頼する
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# posh-gitモジュールのインストール
+Install-Module posh-git -Scope CurrentUser
+
+# oh-my-poshモジュールのインストール
+Install-Module oh-my-posh -Scope CurrentUser
+
+# プロファイル「Microsoft.PowerShell_profile.ps1」の作成
+$data = "Import-Module posh-git`r`nImport-Module oh-my-posh`r`nSet-Theme Powerlevel10k-Classic"
+$data | Out-File "$userprofile\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Encoding default
